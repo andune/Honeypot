262e95b7249c66aca1ee83c447e5f9e825571588
Andune <andune.alleria@gmail.com>
2012-12-30 08:08:53 -0800

    fix all honeypot-related command to be protected by 'honeypot.create'

--- src/main/java/com/argo/bukkit/honeypot/CmdHoneypot.java
+++ src/main/java/com/argo/bukkit/honeypot/CmdHoneypot.java
@@ -20,59 +20,63 @@ public class CmdHoneypot implements CommandExecutor {
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String cmdName, String[] args) {
-    	if(args.length == 0) {
-    		if(sender instanceof Player) {
-    			Player player = (Player)sender;
-    			if( !plugin.hasPermission(sender, "honeypot.create") ) {
-    				player.sendMessage(ChatColor.RED + "You are not allowed to use this command.");
-    			} else {
-    				if(Honeyfarm.getPotSelect(player)) {
-    					player.sendMessage(ChatColor.GREEN + "Honeypot creation finished.");
-    					Honeyfarm.setPotSelect(player, false);
-    				} else {
-    					player.sendMessage(ChatColor.GREEN + "Right click a block with a " + plugin.getHPConfig().getToolId() + " to create a honeypot. When finished, use /hp again.");
-    					Honeyfarm.setPotSelect(player, true);
-    				}
-    			}
-    		} else {
-    			sender.sendMessage("Sorry, this command can only be used by players.");
-    		}
-    	} else if(args.length == 1) {
-    		if( args[0].equalsIgnoreCase("region") ) {
-    			if( sender instanceof Player ) {
-    				Player player = (Player) sender;
-    				Plugin plug = plugin.getServer().getPluginManager().getPlugin("WorldEdit");
-    				if( plug != null ) {
-    					WorldEditPlugin worldEdit = (WorldEditPlugin) plug;
-    					try {
-    						Region region = worldEdit.getSession(player).getSelection(worldEdit.getSession(player).getSelectionWorld());
-    						CuboidRegion cuboidRegion = new CuboidRegion(region, player.getWorld());
-    						Honeyfarm.createPot(cuboidRegion);
-    						Honeyfarm.saveData();
-    						sender.sendMessage(ChatColor.DARK_RED+"WorldEdit region recorded as a Honeypot");
-    					}
-    					catch(IncompleteRegionException ire) {
-    						sender.sendMessage(ChatColor.DARK_RED+"WorldEdit region incomplete");
-    					}
-    				}
-    			}
-    		}
-    		else if(args[0].equalsIgnoreCase("save") || args[0].equalsIgnoreCase("s")) {
-    			sender.sendMessage(ChatColor.GREEN + "Saving honeypot data...");
-    			if(!Honeyfarm.saveData()) {
-    				sender.sendMessage(ChatColor.DARK_RED + "Failed to save data.");
-    			}
-    		}
-    		// added to allow live-reloading of data file  -morganm 5/20/11
-    		else if(args[0].equalsIgnoreCase("reload") || args[0].equalsIgnoreCase("r")) {
-    			sender.sendMessage(ChatColor.GREEN + "Reloading honeypot data from saved file...");
-    			
-    			if(!Honeyfarm.refreshData()) {
-    				sender.sendMessage(ChatColor.DARK_RED + "Failed to load data.");
-    			}
-    		}
-    	}
-    	return true;
+        // must have 'create' permission to access any commands
+        if( !plugin.hasPermission(sender, "honeypot.create") ) {
+            sender.sendMessage(ChatColor.RED + "You are not allowed to use this command.");
+            return true;
+        }
+        
+        if(args.length == 0) {
+            if(sender instanceof Player) {
+                Player player = (Player)sender;
+                if(Honeyfarm.getPotSelect(player)) {
+                    player.sendMessage(ChatColor.GREEN + "Honeypot creation finished.");
+                    Honeyfarm.setPotSelect(player, false);
+                } else {
+                    player.sendMessage(ChatColor.GREEN + "Right click a block with a " + plugin.getHPConfig().getToolId() + " to create a honeypot. When finished, use /hp again.");
+                    Honeyfarm.setPotSelect(player, true);
+                }
+            } else {
+                sender.sendMessage("Sorry, this command can only be used by players.");
+            }
+        } else if(args.length == 1) {
+            if( args[0].equalsIgnoreCase("region") ) {
+                if( sender instanceof Player ) {
+                    Player player = (Player) sender;
+                    Plugin plug = plugin.getServer().getPluginManager().getPlugin("WorldEdit");
+                    if( plug != null ) {
+                        WorldEditPlugin worldEdit = (WorldEditPlugin) plug;
+                        try {
+                            Region region = worldEdit.getSession(player).getSelection(worldEdit.getSession(player).getSelectionWorld());
+                            CuboidRegion cuboidRegion = new CuboidRegion(region, player.getWorld());
+                            Honeyfarm.createPot(cuboidRegion);
+                            Honeyfarm.saveData();
+                            sender.sendMessage(ChatColor.DARK_RED+"WorldEdit region recorded as a Honeypot");
+                        }
+                        catch(IncompleteRegionException ire) {
+                            sender.sendMessage(ChatColor.DARK_RED+"WorldEdit region incomplete");
+                        }
+                    }
+                } else {
+                    sender.sendMessage("Sorry, this command can only be used by players.");
+                }
+            }
+            else if(args[0].equalsIgnoreCase("save") || args[0].equalsIgnoreCase("s")) {
+                sender.sendMessage(ChatColor.GREEN + "Saving honeypot data...");
+                if(!Honeyfarm.saveData()) {
+                    sender.sendMessage(ChatColor.DARK_RED + "Failed to save data.");
+                }
+            }
+            // added to allow live-reloading of data file  -morganm 5/20/11
+            else if(args[0].equalsIgnoreCase("reload") || args[0].equalsIgnoreCase("r")) {
+                sender.sendMessage(ChatColor.GREEN + "Reloading honeypot data from saved file...");
+
+                if(!Honeyfarm.refreshData()) {
+                    sender.sendMessage(ChatColor.DARK_RED + "Failed to load data.");
+                }
+            }
+        }
+        return true;
     }
 
 }
